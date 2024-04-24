33f291a49393a149f776c3ccb59fbf6663b9e263
Nestor Laborier <nestor.laborier@hotmail.fr>
2024-04-24 15:34:33 +0200

    introduce stations groups (paris and lyon)

--- api_links.py
+++ api_links.py
@@ -1,29 +1,39 @@
 import requests
 import pickle
 
-ParisLyon_api_links = [
-    'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=origine%3A%22PARIS%20(intramuros)%22&refine=destination%3A%22LYON%20(intramuros)%22&refine=od_happy_card%3A%22OUI%22',
-    'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=destination%3A%22LYON%20(intramuros)%22&refine=origine%3A%22MARNE%20LA%20VALLEE%20CHESSY%22&refine=od_happy_card%3A%22OUI%22',
-    'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=destination%3A%22LYON%20(intramuros)%22&refine=od_happy_card%3A%22OUI%22&refine=origine%3A%22MASSY%20TGV%22',
-]
+# ParisLyon_api_links = [
+#     'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=origine%3A%22PARIS%20(intramuros)%22&refine=destination%3A%22LYON%20(intramuros)%22&refine=od_happy_card%3A%22OUI%22',
+#     'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=destination%3A%22LYON%20(intramuros)%22&refine=origine%3A%22MARNE%20LA%20VALLEE%20CHESSY%22&refine=od_happy_card%3A%22OUI%22',
+#     'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=destination%3A%22LYON%20(intramuros)%22&refine=od_happy_card%3A%22OUI%22&refine=origine%3A%22MASSY%20TGV%22',
+# ]
 
-LyonParis_api_links = [
-    'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=origine%3A%22LYON%20(intramuros)%22&refine=destination%3A%22PARIS%20(intramuros)%22&refine=od_happy_card%3A%22OUI%22',
-    'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=origine%3A%22LYON%20(intramuros)%22&refine=destination%3A%22MARNE%20LA%20VALLEE%20CHESSY%22&refine=od_happy_card%3A%22OUI%22',
-    'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=od_happy_card%3A%22OUI%22&refine=origine%3A%22LYON%20(intramuros)%22&refine=destination%3A%22MASSY%20TGV%22',
-]
+# LyonParis_api_links = [
+#     'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=origine%3A%22LYON%20(intramuros)%22&refine=destination%3A%22PARIS%20(intramuros)%22&refine=od_happy_card%3A%22OUI%22',
+#     'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=origine%3A%22LYON%20(intramuros)%22&refine=destination%3A%22MARNE%20LA%20VALLEE%20CHESSY%22&refine=od_happy_card%3A%22OUI%22',
+#     'https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=od_happy_card%3A%22OUI%22&refine=origine%3A%22LYON%20(intramuros)%22&refine=destination%3A%22MASSY%20TGV%22',
+# ]
+
+stations_groups = {
+    'GRAND_PARIS' : ["PARIS (intramuros)", "MASSY TGV", "MARNE LA VALLEE CHESSY"],
+    'GRAND_LYON' : ["LYON (intramuros)", "LYON-SAINT EXUPERY TGV"],
+}
 
 api_prefix = "https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?limit=-1&refine=od_happy_card%3A%22OUI%22"
 
 stations_link = "https://ressources.data.sncf.com/api/explore/v2.1/catalog/datasets/tgvmax/records?select=origine&group_by=origine&limit=-1"
-
 
 def api_format(name):
     api_name = "%3A%22" + name.replace(" ","%20") + "%22"
     return api_name
 
 class Stations:
-    def __init__(self) -> None:
+    def __init__(self, list=[]) -> None:
+        self.list = list
+        self.api_names = {}
+        for station in self.list :
+            self.api_names[station] = api_format(station) 
+
+    def get_stations(self) :
         try : 
             response = requests.get(stations_link).json()
             stations = []
@@ -44,19 +54,29 @@ class Stations:
             string += f"{i}".rjust(4) + f" | {station} \n" 
         return string
 
-    def api_link_from_stations(self, org, dest):
-        if org not in self.list or dest not in self.list :
-            raise ValueError("No such station")
-        org_api     = self.api_names[org]
-        dest_api    = self.api_names[dest]
-        return api_prefix + "&refine=origine" + org_api + "&refine=destination" + dest_api
-         
-
+    def api_links_from_stations(self, orgs, dests):
+        api_links = []
+        for org in orgs:
+            for dest in dests:
+                if org not in self.list or dest not in self.list :
+                    raise ValueError("No such station" + org + dest)
+                org_api     = self.api_names[org]
+                dest_api    = self.api_names[dest]
+                api_links.append(api_prefix + "&refine=origine" + org_api + "&refine=destination" + dest_api)
+        return api_links
+    
+    def search(self, request):
+        matching = [station for station in self.list if request in station]
+        return Stations(matching)
 
 if __name__ == "__main__":
     S = Stations()
+    S.get_stations()
     
     with open("__pycache__/stations_backup.pickle",'wb') as f :
         pickle.dump(S.list, f)
 
-    print(S.api_link_from_stations("LYON (intramuros)", "PARIS (intramuros)"))
+    print(S)
+    print(" --- GARES SAUVEGARDEES --- \n")
+
+    print(S.search("LYON"))
--- main.py
+++ main.py
@@ -3,18 +3,36 @@ from api_links import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dest',  dest='destination', type=str, default='paris')
+parser.add_argument('--orig',  dest='origin'     , type=str, default='LYON (intramuros)')
+parser.add_argument('--dest',  dest='destination', type=str, default='PARIS (intramuros)')
 parser.add_argument('--date',  dest='date'       , type=str, default='')
 parser.add_argument('--fhour', dest='fhour'      , type=str, default='')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    if args.destination == 'paris':
-        output = TrajetsTGVmax()
-        output.get(LyonParis_api_links)
-    if args.destination == 'lyon':
-        output = TrajetsTGVmax()
-        output.get(ParisLyon_api_links)
+    stations = Stations()
+    stations.get_stations()
+    output = TrajetsTGVmax()
+    
+    if args.origin in stations_groups :
+        origins = stations_groups[args.origin]
+    else : 
+        origins = [args.origin]
+    
+    if args.destination in stations_groups :
+        destinations = stations_groups[args.destination]
+    else : 
+        destinations = [args.destination]
+
+    api_links = stations.api_links_from_stations(origins, destinations)
+    
+    output.get(api_links)
+    # if args.destination == 'paris':
+    #     output = TrajetsTGVmax()
+    #     output.get(LyonParis_api_links)
+    # if args.destination == 'lyon':
+    #     output = TrajetsTGVmax()
+    #     output.get(ParisLyon_api_links)
     if args.date != '':
         dates = args.date.split('+')
         for date in dates :
